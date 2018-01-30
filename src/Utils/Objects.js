68584668724f82eee08b657963e4f867912f683e
jeremydownes <jeremydownes@hotmail.com>
2018-01-30 17:31:46 -0500

    changes readme, title, and favicon

--- src/Utils/Objects.js
+++ src/Utils/Objects.js
@@ -1,2 +1,18 @@
-export const Objects = [	{location: [1,1], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [0,7], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [3,5], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [9,14], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [12,14], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [20,20], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [1,19], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [5,3], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [17,16], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [15,17], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [6,12], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [7,6], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [15,0], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},{location: [19,13], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},	{location: [17, 8], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}}]
+export const Objects = [	
+	{location: [1,1], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [0,7], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [3,5], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [9,14], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [12,14], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [20,20], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [1,19], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [5,3], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [17,16], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [15,17], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [6,12], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [7,6], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [15,0], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},
+	{location: [19,13], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}},	
+	{location: [17, 8], imgSrc: './coin.png', type: 'coin', interact: {coin: 1, remove: true}}
+]
 export default Objects
